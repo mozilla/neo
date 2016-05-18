2481b07b019815cda1444ed892a2c41ae5b38411
Eli Perelman <eli@eliperelman.com>
2016-05-18 12:17:45 -0500

    Adding missing build command (#8)

---
+++ commands/build/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const prodConfig = require('../../config/webpack.prod');
+const webpack = require('webpack');
+
+module.exports = (args, done) => {
+  let config = args.options.config ?
+    require(path.resolve(process.cwd(), args.options.config)) :
+    prodConfig;
+
+  webpack(config, (err, stats) => {
+    if (!err) {
+      console.log(stats.toString({ colors: true }));
+    } else {
+      console.error(err.stack || err);
+
+      if (err.details) {
+        console.error(err.details);
+      }
+    }
+
+    done();
+  });
+};
