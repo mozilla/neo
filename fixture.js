643f9b6aa84359c1a1e717e84d730c1d0a9c0704
Eli Perelman <eli@eliperelman.com>
2016-05-26 10:02:41 -0500

    Enable integration test and travis again (#17)

---
+++ fixture.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const helpers = require('yeoman-test');
+const path = require('path');
+
+helpers
+  .run(path.join(__dirname, 'commands/init'))
+  .withArguments(['init'])
+  .withOptions({
+    appTitle: 'Test App',
+    packageName: 'test-app',
+    author: 'Mozilla',
+    description: 'Mozilla Test App',
+    port: 5500
+  })
+  .toPromise()
+  .then(dir => {
+    let pkgPath = path.join(dir, 'package.json');
+    let pkg = require(pkgPath);
+
+    pkg.dependencies['mozilla-neo'] = `file:${__dirname}`;
+    fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
+
+    process.stdout.write(dir);
+    process.exit(0);
+  });
